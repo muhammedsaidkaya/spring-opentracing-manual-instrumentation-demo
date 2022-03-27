735dc3a8fa6c2289942d930e0728583cd9642216
Muhammed Kaya <muhammed.kaya@kloia.com>
2022-03-27 17:40:29 +0300

    use opentelemetry-zipkin-exporter

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mvn clean install package -DskipTests
-cp opentelemetry-javaagent-all.jar print-service/
+cp opentelemetry-javaagent.jar print-service/
+cp opentelemetry-javaagent.jar auth-service/
 docker-compose up --build
